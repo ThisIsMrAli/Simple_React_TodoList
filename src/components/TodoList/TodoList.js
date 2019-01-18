4790e09632e283d6cc0a1b906d5f7e0582f2a7b8
CreativeMindW <a.yousefi@airinmedia.com>
2019-01-18 10:01:26 +0330

    added filters

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -9,6 +9,8 @@ import { toggleTodoStatus, removeTodo } from './../../store/actions';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import ClearIcon from '@material-ui/icons/Clear';
+import TodoListFilter from './../TodoListFilter/TodoListFilter';
+import cssStyles from './TodoList.module.css';
 
 const styles = theme => ({
     root: {
@@ -18,32 +20,56 @@ const styles = theme => ({
         backgroundColor: theme.palette.background.paper,
     },
 });
+const FILTERS = {
+    ALL: "ALL",
+    COMPELETED: 'COMPELETED',
+    INCOMPELETED:'INCOMPELETED'
+}
 
 class TodoList extends Component {
+    state = {
+        activeFilter: FILTERS.ALL,
+        filters: [
+            FILTERS.ALL,
+            FILTERS.COMPELETED,
+            FILTERS.INCOMPELETED
+        ]
+    }
     handleToggle = (id) => {
         this.props.toggleTodo(id);
     }
     handleRemoveClick = (id) => {
         this.props.removeTodo(id);
     }
+    handleFilterChange = (filter) => {
+        this.setState({
+            activeFilter: filter
+        })
+    }
     render() {
         const { classes } = this.props;
+        console.log(this.state.activeFilter === FILTERS.ALL)
+        const filteredTodos = this.state.activeFilter === FILTERS.ALL? this.props.todos:
+        this.props.todos.filter(todo => this.state.activeFilter == FILTERS.COMPELETED ? todo.compeleted : !todo.compeleted)
         return (
-            <List className={classes.root}>
-                {this.props.todos.map(todo => <ListItem key={todo.id} role={undefined} dense button onClick={() => this.handleToggle(todo.id)}>
-                    <Checkbox
-                        checked={todo.compeleted}
-                        tabIndex={-1}
-                        disableRipple
-                    />
-                    <ListItemText primary={todo.text} />
-                    <ListItemSecondaryAction>
-                        <IconButton aria-label="remove" onClick={() => this.handleRemoveClick(todo.id)}>
-                            <ClearIcon  />
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>)}
-            </List>
+            <div className={cssStyles['list-root']}>
+                <List className={classes.root}>
+                    {filteredTodos.map(todo => <ListItem key={todo.id} role={undefined} dense button onClick={() => this.handleToggle(todo.id)}>
+                        <Checkbox
+                            checked={todo.compeleted}
+                            tabIndex={-1}
+                            disableRipple
+                        />
+                        <ListItemText primary={todo.text} />
+                        <ListItemSecondaryAction>
+                            <IconButton aria-label="remove" onClick={() => this.handleRemoveClick(todo.id)}>
+                                <ClearIcon />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>)}
+                </List>
+                <TodoListFilter handleChange={this.handleFilterChange} filters={this.state.filters} activeFilter={this.state.activeFilter} />
+            </div>
         )
     }
 }
---
+++ src/components/TodoListFilter/TodoListFilter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+
+const TodoListFilter = (props) => {
+    const { handleChange, activeFilter, filters } = props;
+    return (
+        <FormControl component="fieldset" >
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+                aria-label="filter Todos"
+                name="filter"
+                value={activeFilter}
+                row
+                onChange={(event) => handleChange(event.target.value)}
+            >
+                {filters.map(filter => (<FormControlLabel value={filter} control={<Radio />} label={filter} />))}
+            </RadioGroup>
+        </FormControl>
+    )
+}
+
+export default TodoListFilter;
